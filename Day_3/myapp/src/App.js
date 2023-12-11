8f5c1ec496a7cf0b95de6000c8c5b136f6697810
VAISHNAVI M <vaishnavim1194@gmail.com>
2023-12-11 19:29:09 +0530

    Day3(React)

---
+++ Day_3/myapp/src/App.js
@@ -0,0 +1,16 @@
+import Nav from "./Components/Nav";
+import "./Assets/CSS/nav.css";
+import Home from "./Components/Home";
+import "./Assets/CSS/home.css";
+
+function App()
+{
+  return(
+    <>
+    <Nav/>
+    <Home/>
+    </>
+  )
+}
+
+export default App;
---
+++ Day_3/myapp/src/Components/Nav.js
@@ -0,0 +1,41 @@
+
+// function Nav() Normal Function
+// {
+
+// }
+
+// const Nav =()=>   Arrow Function
+// {
+
+// }
+
+
+function Nav()
+{
+    return(
+        <> 
+            <nav>
+            {/* <div class="nav__logo">
+                <a href="#"><img src="header logo.png" alt="logo" /></a>
+            </div> */}
+            <ul class="nav__links">
+                <li class="link"><a href="#">UPSCRIPTION FLOWERS</a></li>
+                <li class="link"><a href="#">LETTERBOX FLOWERS</a></li>
+                <li class="link"><a href="#">OCCASIONS</a></li>
+                <li class="link"><a href="#">HAND-TIED BOUQUETS</a></li>
+                <li class="link"><a href="#">PLANTS</a></li>
+                <li class="link"><a href="#">GIFTS</a></li>
+                <li class="link"><a href="#">OUR ETHICS</a></li>
+                <li class="link"><a href="#">ALL PRODUCTS</a></li>
+            </ul>
+            <div class="cart">
+                <a href="#"><i class=""></i></a>
+            </div>
+        </nav>
+        </>
+
+
+    )
+}
+
+export default Nav;
